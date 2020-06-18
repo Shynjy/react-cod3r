f2b39433be977d3f55e0ddc39dec4b7ca43e75c3
Shynjy <furia.designer@gmail.com>
2020-06-18 17:31:26 -0300

    update Calculator.jsx

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -35,6 +35,21 @@ export default class Calculator extends Component {
         if (n === '.' && this.state.displayValue.includes('.')) {
             return
         }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+            console.log(clearDisplay);            
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValeu = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValeu
+            this.setState({ values })        
+        }
     }
 
     render() {

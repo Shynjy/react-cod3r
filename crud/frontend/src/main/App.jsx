89c5fc2cd8924f4ae8f0f8536b1d5ad5dd42d82a
Shynjy <furia.designer@gmail.com>
2020-06-22 15:14:20 -0300

    frontend ended

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,15 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css"
 import React from "react"
+import { HashRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom' --> Funciona como o HasRouter porém o servidor tem que ser arrumado.
 
 import Logo from "../components/template/Logo"
 import Nav from "../components/template/Nav"
-import Main from "../components/template/Main"
+import Routes from './Routes'
 import Footer from "../components/template/Footer"
 
-export default props => 
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main />
-        <Footer />
-    </div>
+export default props =>
+    <HashRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </HashRouter>
